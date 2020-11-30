a2271c8bddc7230d94e9158bba9bfe76b2ddc6df
leachs18 <samleach2018@gmail.com>
2020-11-30 17:24:02 -0500

    table changes

--- covidmap/src/components/Quick_stats.vue
+++ covidmap/src/components/Quick_stats.vue
@@ -1,7 +1,5 @@
 <template>
-  <span v-html="town">
-    test
-  </span>
+  <span v-html="town"> </span>
 </template>
 
 
@@ -30,7 +28,7 @@ export default{
                 console.log(res.data);
                 const towns = res.data;
                 var head = '<table>'
-                var title = '<tr><th style=padding-right:10px;>Name</th><th style=padding-right:10px;>Positive Cases</th><th style=padding-right:10px>Number Tested</th><th style=padding-right:10px>Zip Code</th><th style=style=padding-right:10px>Percent Positive</th></tr>' //copy and paste the title
+                var title = '<tr><th style=background-color:lime;padding-right:10px>Name</th><th style=background-color:lime;padding-right:10px>Positive Cases</th><th style=background-color:lime;padding-right:10px>Number Tested</th><th style=background-color:lime;padding-right:10px>Zip Code</th><th style=background-color:lime;padding-right:10px>Percent Positive</th></tr>' //copy and paste the title
                 var middle = ''
                 for (var i = 0; i < towns.length; i++){
                   var name = towns[i].name;
@@ -39,13 +37,12 @@ export default{
                   var zip = towns[i].zip_code;
                   var percent = towns[i].percent_positive;
 
-                  middle += `<tr><td>${name}</td><td>${positive_cases}</td><td>${tested}</td><td>${zip}</td><td>${percent}</td></tr>`
+                  middle += `<tr><td style=border:1px;border-color:black;border-width:1px;border-style:solid>${name}</td><td style=border:1px;border-color:black;border-width:1px;border-style:solid>${positive_cases}</td><td style=border:1px;border-color:black;border-width:1px;border-style:solid>${tested}</td><td style=border:1px;border-color:black;border-width:1px;border-style:solid>${zip}</td><td style=border:1px;border-color:black;border-width:1px;border-style:solid>${percent}</td></tr>`
                 }
                 var bottom = '</table>'
-                var content = head + title + middle + bottom;
-                console.log(content);
-                this.town.push({content});
-                console.log(this.town);
+                var test = head + title + middle + bottom;
+                console.log(test);
+                this.town = test
         })
     },
   },
